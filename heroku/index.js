118a68d71749f3cf7217eee6766ad3350ec2ad05
unknown <shahwaiz055@gmail.com>
2018-11-22 16:12:55 +0500

    make it better

--- heroku/index.js
+++ heroku/index.js
@@ -14,10 +14,14 @@ var xhub = require('express-x-hub');
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
 
-app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
+//app secret : 7eebbaaad989b6d8a05657d1d47d803f
+//token :      12dwrdsfaf2332j3iom32n3ewbr3b33b 
+
+
+app.use(xhub({ algorithm: 'sha1', secret:  "7eebbaaad989b6d8a05657d1d47d803f"/*process.env.APP_SECRET*/ }));
 app.use(bodyParser.json());
 
-var token = process.env.TOKEN || 'token';
+var token = "12dwrdsfaf2332j3iom32n3ewbr3b33b"//process.env.TOKEN || 'token';
 var received_updates = [];
 
 app.get('/', function(req, res) {
